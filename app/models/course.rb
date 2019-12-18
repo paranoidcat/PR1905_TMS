5c346aa918b1d6cee080a53daa09a8e20267e620
Dao Van Quynh <hoadaocusi@gmail.com>
2019-12-18 20:39:30 +0700

    CRUD Coure

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ApplicationRecord
+  
+  validates :name,  presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :introduction,  presence: true, length: { maximum: 1000 }
 end
